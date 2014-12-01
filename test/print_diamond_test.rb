cb1cdf761ba05db25f6dd2d7db00c8c59bdbfd45
Steve Tooke <steve@boxjump.co.uk>
2014-12-01 16:35:25 +0000

    Add a test for printing B
    Bit of a nasty hack to unindent the heredoc, My bad.

--- test/print_diamond_test.rb
+++ test/print_diamond_test.rb
@@ -1,10 +1,25 @@
 require 'minitest/autorun'
 require 'print_diamond'
 
+class String
+  def unindent
+    gsub /^#{scan(/^[ \t]*\b/).min}/,''
+  end
+end
+
 class TestPrintDiamond < MiniTest::Unit::TestCase
   include PrintDiamond
 
   def test_print_a
     assert_equal 'A', print_diamond('A')
+  end
+
+  def test_print_b
+    expected = <<-END.unindent.chomp
+     A 
+    B B
+     A 
+    END
+    assert_equal expected, print_diamond('B')
   end
 end
