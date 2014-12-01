ddb9343bd6e897f27f30be6ead1a4177d63d2f39
Steve Tooke <steve@boxjump.co.uk>
2014-12-01 16:45:37 +0000

    Create print line method
    I saw some sort of pattern on the different lines that I wanted to
    extract. This method kind of explains that.

--- lib/print_diamond.rb
+++ lib/print_diamond.rb
@@ -1,4 +1,6 @@
 module PrintDiamond
+  ALPHABET = Array('A'..'Z')
+
   def print_diamond(letter)
     if letter == 'A'
       ['A']
@@ -7,5 +9,14 @@ module PrintDiamond
     else
       ['  A  ', ' B B ', 'C   C', ' B B ', '  A  ']
     end.join("\n")
+  end
+
+  private
+  def print_line(letter)
+    if letter == 'A'
+      letter
+    else
+      [letter, ' ' * ALPHABET.index(letter), letter].join
+    end
   end
 end
